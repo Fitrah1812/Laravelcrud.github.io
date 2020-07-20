75e0dc05b31e0b0fbefd4760e07a57da86ed91bb
Fitrah1812 <arieramadhanfitrah@gmail.com>
2020-07-20 10:50:25 +0700

    Laravel Crud

---
+++ python/mantap.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+# print("Hello World")
+# print("Hello hello")
+z=0
+z= input()
+print(z)
+a=0
+a = z+z+z*z+z+z*z-z
+print(a)
+# if (int z>2 & int z<= 5 & int z%2 == 0) :
+#     print("Not Weird\n")
+# elif(int z>5 & int z<=20 & int z%2==0) :
+#     print("Weird\n")
+# elif(int z>20 & int z%2==0) :
+#     print("Weird\n")
+# else :
+#     print("Weird\n")
